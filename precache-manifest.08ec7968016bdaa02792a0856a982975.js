dfe0ef319d16cfe3bba9b2290ba82a0152c5e4df
razifharagi13 <razif_haragi@yahoo.com>
2019-10-11 18:26:56 +0700

    Updates

---
+++ precache-manifest.08ec7968016bdaa02792a0856a982975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3013b86ce2355b87966132e616f8f4d",
+    "url": "/paolatest/index.html"
+  },
+  {
+    "revision": "e0a9678d5a0cc48baee6",
+    "url": "/paolatest/static/css/main.a1965e2d.chunk.css"
+  },
+  {
+    "revision": "31902f8f0301d5b0639e",
+    "url": "/paolatest/static/js/2.0520baf7.chunk.js"
+  },
+  {
+    "revision": "e0a9678d5a0cc48baee6",
+    "url": "/paolatest/static/js/main.663d13df.chunk.js"
+  },
+  {
+    "revision": "61b83a89392de247ab34",
+    "url": "/paolatest/static/js/runtime-main.035b618d.js"
+  },
+  {
+    "revision": "f12333e614837996df1ec3424252078d",
+    "url": "/paolatest/static/media/bg.f12333e6.png"
+  }
+]);
